dfe3a4e1d0fef7cdecbda0307550a8ca83d3475d
dhruvchavda05 <dhruvbchavda@gmail.com>
2022-02-22 14:56:03 +0530

    Counter

---
+++ Day 4/Task 4/task4.js
@@ -0,0 +1,6 @@
+function show(){
+    var name = document.getElementById("fname").value;
+    var city = document.getElementById("city").value;
+    document.getElementById("answer").innerHTML=name;
+    document.getElementById("answer2").innerHTML=city;
+}
