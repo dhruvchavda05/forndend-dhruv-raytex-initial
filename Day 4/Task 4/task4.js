9e3981a5554d48d7e8a44ff76ea85150bbf865e0
Dhruv Chavda <63991338+dhruvchavda05@users.noreply.github.com>
2022-02-22 15:49:05 +0530

    Update task4.js
    More values in Table

--- Day 4/Task 4/task4.js
+++ Day 4/Task 4/task4.js
@@ -3,4 +3,7 @@ function show(){
     var city = document.getElementById("city").value;
     document.getElementById("answer").innerHTML=name;
     document.getElementById("answer2").innerHTML=city;
+    let table = document.getElementById("tablebody");
+    
+    table.innerHTML = table.innerHTML + "<tr><td>" + name +"</td></tr>" +"<tr><td>" + city +"</td></tr>";
 }
